652cfede317d09989d3f79876cbac0b54c7a7424
Vidar Eldøy <vidar@eldoy.com>
2022-04-18 02:40:49 +0200

    Fix runner

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const path = require('path')
 module.exports = async function(name, params = {}) {
   const file = path.join(process.cwd(), 'spec', 'tests', `${name}.test.js`)
   const tests = require(file)
-  for (const name in tests) {
+  for (const test in tests) {
     try {
-      return await tests[name](params)
+      await tests[test](params)
     } catch (e) {
-      console.log(`\n* it['${name}'] in '${file}' failed:\n`)
+      console.log(`\n* it['${test}'] in '${name}' failed:\n`)
       console.log(e.message)
       console.log()
+      return
     }
   }
 }
